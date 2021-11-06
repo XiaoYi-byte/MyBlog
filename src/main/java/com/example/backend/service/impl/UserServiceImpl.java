8297d33b905a369aa4424f159dd521548a613e22
Kuark <72679319+XiaoYi-byte@users.noreply.github.com>
2021-11-06 18:48:22 +0800

    add interceptor

--- src/main/java/com/example/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/backend/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.backend.service.impl;
 import com.example.backend.dao.UserRepository;
 import com.example.backend.entity.User;
 import com.example.backend.service.UserService;
+import com.example.backend.util.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User checkUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, MD5Utils.code(password));
         return user;
     }
 }
